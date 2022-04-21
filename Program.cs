df8b9f28b92278d728681150eaa2d341396a16f3
BBKIY <104115351+BBKIY@users.noreply.github.com>
2022-04-21 05:44:22 +0300

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int growth = 181;
+            
+            sbyte age;
+            age = 21;
+
+            byte gold = 100;
+            short level = 45;
+            ushort count = 0;
+            uint speed = 50;
+            long time = 15;
+            ulong distance = 300;
+            double rating = 200;
+            case health = 100;
+
+        }
+    }
+}
